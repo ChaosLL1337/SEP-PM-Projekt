362bcf93a261ffd85881ee58ce7339026ef526d7
Paulus <3012775@stud.hs-mannheim.de>
2025-09-25 13:12:20 +0200

    blabla

--- app/src/main/java/com/example/tut2/MainActivity.kt
+++ app/src/main/java/com/example/tut2/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.example.tut2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.method.ScrollingMovementMethod
 import android.view.View
-import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,21 +12,16 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
     private lateinit var startScreen: View
     private lateinit var chatScreen: View
-
     private lateinit var etIn: EditText
     private lateinit var btnSend: ImageButton
-
     private lateinit var chatRecycler: RecyclerView
-
     private lateinit var btnBack: ImageButton
-
     private val conversation = mutableListOf<Message>()
     private lateinit var chatAdapter: ChatAdapter
 
 
 
-
-
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -48,7 +38,6 @@ class MainActivity : AppCompatActivity() {
         chatAdapter = ChatAdapter(conversation)
         chatRecycler.adapter = chatAdapter
         chatRecycler.layoutManager = LinearLayoutManager(this)
-
 
 
 
@@ -87,21 +76,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
-//        chatSend.setOnClickListener {
-//            val userMsg = chatInput.text.toString().trim()
-//            if (userMsg.isNotEmpty()) {
-//                conversation.add(Message("User", userMsg))
-//                chatInput.text.clear()
-//
-//                val botReply = "Antwort auf: $userMsg"
-//                conversation.add(Message("Bot", botReply))
-//
-//                chatAdapter.notifyDataSetChanged()
-//                chatRecycler.scrollToPosition(conversation.size - 1)
-//            }
-//        }
-
         btnBack.setOnClickListener {
             chatScreen.visibility = View.GONE
             startScreen.visibility = View.VISIBLE
@@ -112,22 +86,4 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    private fun appendMessage(sender: String, message: String) {
-        // Neue Nachricht zur Liste hinzufügen
-        conversation.add(Message(sender, message))
-
-        // Adapter benachrichtigen, dass sich Daten geändert haben
-        chatAdapter.notifyItemInserted(conversation.size - 1)
-
-        // Scrollen zum letzten Eintrag
-        chatRecycler.scrollToPosition(conversation.size - 1)
-    }
-
-
-
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        super.onSaveInstanceState(outState)
-//        outState.putString("conversation", conversation.toString())
-//        outState.putString("input", etIn.text.toString())
-//    }
 }
